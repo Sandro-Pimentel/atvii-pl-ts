e5b7fa59acd55a5c6955f8ddb77c77362b500052
Sandro-Pimentel <125677890+Sandro-Pimentel@users.noreply.github.com>
2024-06-10 21:41:43 -0300

    Update barraNavegacao.tsx

--- src/componentes/barraNavegacao.tsx
+++ src/componentes/barraNavegacao.tsx
@@ -22,17 +22,6 @@ export default class BarraNavegacao extends Component<props>{
             return <></>
         } else {
             let lista = this.props.botoes.map((valores, index) =>
-                // <li class="nav-item dropdown">
-                //     <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                //         Dropdown
-                //     </a>
-                //     <ul class="dropdown-menu">
-                //         <li><a class="dropdown-item" href="#">Action</a></li>
-                //         <li><a class="dropdown-item" href="#">Another action</a></li>
-                //         <li><hr class="dropdown-divider"></li>
-                //         <li><a class="dropdown-item" href="#">Something else here</a></li>
-                //     </ul>
-                // </li>
                 <li key={index} className="nav-item dropdown">
                     <a className="nav-link dropdown-toggle" href="#"  role="button" data-bs-toggle="dropdown" aria-expanded="false">{this.props.titulos[index]}</a>
                     <ul className="dropdown-menu">
